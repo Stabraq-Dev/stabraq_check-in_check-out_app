bb47b2a00326d1645a6b4ff4769ebd8440aa943c
mero2online <mero2online@gmail.com>
2021-10-12 01:46:21 +0200

    EDIT: Handel Splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -6,15 +6,27 @@ import history from '../history';
 import { doShrinkLogo } from '../actions';
 
 function Splash(props) {
+  useEffect(() => props.doShrinkLogo(false));
   const shrinkLogo =
     props.shrinkLogo || history.location.pathname !== '/' ? 'shrink-logo' : '';
+
+  const redirectTo = () => {
+    if (history.location.pathname === '/') {
+      return '/preferences/main';
+    } else if (history.location.pathname === '/preferences/main') {
+      return '/';
+    } else {
+      return history.location.pathname.replace(/\/([^/]+)\/?$/, '');
+    }
+  };
+
   return (
-    <Link to={props.isSignedIn ? '/preferences/main/user' : '/dashboard'}>
+    <Link to={props.isSignedIn ? redirectTo : '/dashboard'}>
       <div className='text-center'>
         <button
           className='btn me-2 no-btn-focus'
           type='button'
-          onClick={props.doShrinkLogo}
+          onClick={() => props.doShrinkLogo(true)}
         >
           <img
             className={`mx-auto d-block logo-img ${shrinkLogo}`}
