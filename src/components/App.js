d7c7e4256d01d7bda774c4e40c74c6e6f9b7615a
mero2online <mero2online@gmail.com>
2021-10-11 10:10:54 +0200

    ADD: App components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from '../history';
 
-function App() {
-  return <div>App</div>;
-}
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '../styles.css';
+
+import Splash from './Splash';
+import Preferences from './Preferences';
+import AdminLogInForm from './AdminLogInForm';
+
+const App = () => {
+  return (
+    <div className='ui container mt-3'>
+      <Router history={history}>
+        <div>
+          <Splash />
+          <Switch>
+            <Route path='/dashboard'>
+              <AdminLogInForm />
+            </Route>
+            <Route path='/preferences'>
+              <Preferences />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
