ec76da5a1e3e144e5ac41cb93750a7c8074c11ea
mero2online <mero2online@gmail.com>
2021-10-12 12:39:33 +0200

    EDIT: Change image to icons in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { doShowCheckInOut, doShrinkIcon } from '../actions';
+import { doShowCheckInOut, doShrinkIcon, doLogOut } from '../actions';
+// import { Person, PersonAdd } from '@mui/icons-material';
 
 class Main extends React.Component {
   onFormSubmitUser = async () => {
@@ -15,45 +16,63 @@ class Main extends React.Component {
   };
 
   render() {
-    const shrink =
-      this.props.shrinkIcon || window.location.pathname !== '/preferences/main'
-        ? 'shrink'
-        : '';
+    // const shrink =
+    //   this.props.shrinkIcon || window.location.pathname !== '/preferences/main'
+    //     ? 'shrink'
+    //     : '';
     return (
-      <div>
-        <div className='row ui container mt-3'>
-          <nav className='navbar navbar-light'>
-            <form className='container-fluid justify-content-center'>
-              <Link to='/preferences/main/user'>
-                <button
-                  className='btn btn-outline-success me-2 bg-dark'
-                  type='button'
-                  onClick={this.onFormSubmitUser}
-                >
-                  <img
+      <div className='row mt-3 mb-3'>
+        <div className='col-sm-4 col-xs-12 mt-3 text-center'>
+          <Link to='/preferences/main/user'>
+            <button
+              className='ui text-stabraq button bg-dark'
+              type='button'
+              onClick={this.onFormSubmitUser}
+            >
+              {/* <img
                     className={`mx-auto d-block user-img ${shrink}`}
                     src='/user-member.png'
                     alt='user-member'
-                  />
-                  User
-                </button>
-              </Link>
-              <Link to='/preferences/main/new-user'>
-                <button
-                  className='btn btn-outline-success me-2 bg-dark'
-                  type='button'
-                  onClick={this.onFormSubmitNewUser}
-                >
-                  <img
+                  /> */}
+              {/* <Person
+                sx={{ fontSize: 50 }}
+                className={`user-img ${shrink}`}
+              /> */}
+              <i className='user icon' />
+              User
+            </button>
+          </Link>
+        </div>
+
+        <div className='col-sm-4 col-xs-12 mt-3 text-center'>
+          <Link to='/preferences/main/new-user'>
+            <button
+              className='ui text-stabraq button bg-dark'
+              type='button'
+              onClick={this.onFormSubmitNewUser}
+            >
+              {/* <img
                     className={`mx-auto d-block user-img ${shrink}`}
                     src='/user-new-user.png'
                     alt='user-new-user'
-                  />
-                  New User
-                </button>
-              </Link>
-            </form>
-          </nav>
+                  /> */}
+              {/* <PersonAdd
+                sx={{ fontSize: 50 }}
+                className={`user-img ${shrink}`}
+              /> */}
+              <i className='user plus icon' />
+              New User
+            </button>
+          </Link>
+        </div>
+        <div className='col-sm-4 col-xs-12 mt-3 text-end align-self-center'>
+          <button
+            className='ui red button'
+            onClick={() => this.props.doLogOut()}
+          >
+            <i className='sign-out icon' />
+            Sign Out
+          </button>
         </div>
       </div>
     );
@@ -67,6 +86,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, { doShowCheckInOut, doShrinkIcon })(
-  Main
-);
+export default connect(mapStateToProps, {
+  doShowCheckInOut,
+  doShrinkIcon,
+  doLogOut,
+})(Main);
