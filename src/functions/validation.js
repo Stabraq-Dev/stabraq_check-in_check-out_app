9f66732b09922d196e66ea8a80850cea799cee83
mero2online <mero2online@gmail.com>
2022-02-12 23:29:09 +0200

    EDIT: Enhance validations

--- src/functions/validation.js
+++ src/functions/validation.js
@@ -1,77 +1,71 @@
 export const checkForMobNum = async (inputTerm) => {
   // console.log('::: Running checkForMobNum :::', inputTerm);
-
-  function validMobNum(inputTerm) {
-    let regexp = /^\+?01[0-9]{9}$/;
-    return regexp.test(inputTerm);
-  }
+  const regexpForMobile = /^\+?01[0-9]{9}$/;
 
   if (!inputTerm) {
     return 'Please enter mobile number';
   }
 
-  if (validMobNum(inputTerm)) {
-    return '';
+  if (!regexpForMobile.test(inputTerm)) {
+    return 'Enter valid mobile number';
   }
 
-  return 'Enter valid mobile number';
+  return '';
 };
 
 export const checkForEmail = async (inputTerm) => {
   // console.log('::: Running checkForEmail :::', inputTerm);
-
-  function validEmail(inputTerm) {
-    let regexp =
-      /^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$/g;
-    return regexp.test(inputTerm);
-  }
+  const regexpForNoSpaces = /^\S*$/;
+  const regexpForEmail =
+    /^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$/g;
 
   if (!inputTerm) {
     return 'Please enter e-mail';
   }
 
-  if (validEmail(inputTerm)) {
-    return '';
+  if (!regexpForNoSpaces.test(inputTerm)) {
+    return 'Please delete spaces, Email cannot contain spaces';
   }
 
-  return 'Please enter valid e-mail';
+  if (!regexpForEmail.test(inputTerm)) {
+    return 'Please enter valid e-mail';
+  }
+
+  return '';
 };
 
 export const checkForUserName = async (inputTerm) => {
   // console.log('::: Running checkForUserName :::', inputTerm);
-
-  function validUserName(inputTerm) {
-    let regexp =
-      /^[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z\s]+[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z-_\s]*$/;
-    return regexp.test(inputTerm);
-  }
+  const regexpForSpaceAtBeginning = /^[^-\s]/;
+  const regexpForEngOrAraName =
+    /^[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z\s]+[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z-_\s]*$/;
 
   if (!inputTerm) {
     return 'Please enter user name';
   }
 
-  if (validUserName(inputTerm)) {
-    return '';
+  if (!regexpForSpaceAtBeginning.test(inputTerm)) {
+    return 'Please delete space at the beginning';
   }
 
-  return 'Please enter valid user name';
+  if (!regexpForEngOrAraName.test(inputTerm)) {
+    return 'Please enter valid user name';
+  }
+
+  return '';
 };
 
 export const checkForMembership = async (inputTerm) => {
   // console.log('::: Running checkForMembership :::', inputTerm);
-
-  function validMembership(inputTerm) {
-    let regexp = /\w{1,}/;
-    return regexp.test(inputTerm);
-  }
+  const regexp = /\w{1,}/;
 
   if (inputTerm === undefined) {
     return 'Please select membership';
   }
 
-  if (validMembership(inputTerm)) {
-    return '';
+  if (!regexp.test(inputTerm)) {
+    return 'Please select membership';
   }
 
-  return 'Please select membership';
+  return '';
 };
