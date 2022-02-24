c21225cb3c36d5776386c16c4507e1217d2ea1cd
mero2online <mero2online@gmail.com>
2022-02-24 04:27:49 +0200

    EDIT: Refactor Helper functions

--- src/functions/helperFunc.js
+++ src/functions/helperFunc.js
@@ -2,6 +2,8 @@
  @a Helper Functions 
  */
 
+import { REMAIN_DAYS_EXCEL_FORMULA } from '../ranges';
+
 export const changeDateFormat = async (inputDate) => {
   let date = new Date(inputDate);
 
@@ -58,6 +60,15 @@ export const checkYearDiff = async (dataDateOne, dataDateTwo) => {
   return diffYears;
 };
 
+export const checkDayDiffWithMinus = (dataDateOne, dataDateTwo) => {
+  const dateOne = new Date(dataDateOne);
+  const dateTwo = new Date(dataDateTwo);
+  const diffTime = dateTwo - dateOne;
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+  return diffDays;
+};
+
 export const getByValue = async (arr, value) => {
   let result = [];
 
@@ -81,5 +92,48 @@ export const getData = async (arr, keyToGet) => {
     });
   });
 
-  return result.flat().map((item) => (item.length <= 0 ? '' : item)).flat();
+  return result
+    .flat()
+    .map((item) => (item.length <= 0 ? '' : item))
+    .flat();
+};
+
+export const getExpiryDate = (days) => {
+  let expiry_date = new Date();
+  expiry_date.setDate(expiry_date.getDate() + days);
+  return expiry_date.toLocaleDateString('en-US');
+};
+
+export const calculateUserData = (formValues, lastBlankRow) => {
+  const { membership, hoursPackages } = formValues;
+  const expiryDate =
+    membership === 'HOURS_MEMBERSHIP'
+      ? getExpiryDate(90)
+      : membership === 'NOT_MEMBER'
+      ? ''
+      : getExpiryDate(30);
+
+  const remainDays =
+    membership === 'NOT_MEMBER' ? '' : REMAIN_DAYS_EXCEL_FORMULA(lastBlankRow);
+
+  const remainingHours = membership === 'HOURS_MEMBERSHIP' ? hoursPackages : '';
+
+  const remainingOfTenDays = membership === '10_DAYS' ? 10 : '';
+
+  const invitations =
+    membership === 'GREEN'
+      ? 3
+      : membership === 'ORANGE'
+      ? 5
+      : membership === 'BUSINESS'
+      ? 7
+      : '';
+
+  return {
+    expiryDate,
+    remainDays,
+    remainingHours,
+    remainingOfTenDays,
+    invitations,
+  };
 };
