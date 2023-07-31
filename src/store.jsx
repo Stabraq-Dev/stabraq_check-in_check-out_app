de5806c13b6e7de36979841ed11a7372eed4876d
mero2online <mero2online@gmail.com>
2023-08-01 00:01:41 +0300

    EDIT: Disable immutableCheck in redux

--- src/store.jsx
+++ src/store.jsx
@@ -6,6 +6,7 @@ const store = configureStore({
   reducer: reducers,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
+      immutableCheck:false,
       serializableCheck: false,
     }),
 });
